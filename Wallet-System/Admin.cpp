b92d10d340ac1edcfd9cab7c3dd03a84aa5e7b75
Ree37 <rimo.mrben@gmail.com>
2024-04-16 15:17:58 +0200

    commenting admin code

--- Wallet-System/Admin.cpp
+++ Wallet-System/Admin.cpp
@@ -1,47 +1,47 @@
-#include "Admin.h"
-#include "User.h"
-
-int Admin::get_Index(string user_name){
-    for(int i =0;i<=users.size();i++){
-        if(users[i].name==user_name){
-            return i;
-        }else{
-            continue;
-        }
-
-    }
-}
-void Admin::delete_User(string deleted_User_Name ){
-
-    int position= Admin::get_Index(deleted_User_Name);
-
-   Admin::users.erase(position);
-   unique_Users_Name.erase(deleted_User_Name);
-}
-void Admin::suspend_User(string suspended_User_Name ){
-  int position=  int position= Admin::get_Index(suspended_User_Name);
-     users.at(position).susbended_Flag=true;
-}
-void Admin::adjust_User_Balance(string adjusted_user_name,int value){
-    int position=  int position= Admin::get_Index(suspended_User_Name);
-
-    users.at(position).balance=value
-
-}
-void Admin::view_Users()
-{
-    for (int i = 0; i <= users.size(); i++) {
-        cout << "Username : " << users.at(i).name << "\t Balance : " << users.at(i).balance
-             << endl;
-        for (int j = 0; j < users.at(i).transactions.size();i++)
-        {
-            cout << "transaction"<<j+1<<users.at(i).transactions.at(j);
-        }
-        cout<<"***************************************";
-
-}
-bool Admin::is_Unique(string name){
-
-    return !unique_Users_Name.count(name);
-
-}
+//#include "Admin.h"
+//#include "User.h"
+//
+//int Admin::get_Index(string user_name){
+//    for(int i =0;i<=users.size();i++){
+//        if(users[i].name==user_name){
+//            return i;
+//        }else{
+//            continue;
+//        }
+//
+//    }
+//}
+//void Admin::delete_User(string deleted_User_Name ){
+//
+//    int position= Admin::get_Index(deleted_User_Name);
+//
+//   Admin::users.erase(position);
+//   unique_Users_Name.erase(deleted_User_Name);
+//}
+//void Admin::suspend_User(string suspended_User_Name ){
+//  int position=  int position= Admin::get_Index(suspended_User_Name);
+//     users.at(position).susbended_Flag=true;
+//}
+//void Admin::adjust_User_Balance(string adjusted_user_name,int value){
+//    int position=  int position= Admin::get_Index(suspended_User_Name);
+//
+//    users.at(position).balance=value
+//
+//}
+//void Admin::view_Users()
+//{
+//    for (int i = 0; i <= users.size(); i++) {
+//        cout << "Username : " << users.at(i).name << "\t Balance : " << users.at(i).balance
+//             << endl;
+//        for (int j = 0; j < users.at(i).transactions.size();i++)
+//        {
+//            cout << "transaction"<<j+1<<users.at(i).transactions.at(j);
+//        }
+//        cout<<"***************************************";
+//
+//}
+//bool Admin::is_Unique(string name){
+//
+//    return !unique_Users_Name.count(name);
+//
+//}
--- Wallet-System/Admin.h
+++ Wallet-System/Admin.h
@@ -1,24 +1,24 @@
-#pragma once
-#include<iostream>
-#include<string>
-#include<set>
-#include<vector>
-#include "User.h"
-using namespace std;
-
-class Admin
-{
-string admin_Name;
-string admin_Password;
-vector<User> users;
-set<string> unique_Users_Name;
-bool is_Unique(string name);
-void add_User();
-void view_Users();
-void edit_User(User edited_User);
-void view_Transactions();
-void suspend_User(string suspended_User_Name );
-void delete_User(string deleted_User);
-void adjust_User_Balance(string adjusted_user_name,int value);
-int get_Index(string user_name);
-};
+//#pragma once
+//#include<iostream>
+//#include<string>
+//#include<set>
+//#include<vector>
+//#include "User.h"
+//using namespace std;
+//
+//class Admin
+//{
+//string admin_Name;
+//string admin_Password;
+//vector<User> users;
+//set<string> unique_Users_Name;
+//bool is_Unique(string name);
+//void add_User();
+//void view_Users();
+//void edit_User(User edited_User);
+//void view_Transactions();
+//void suspend_User(string suspended_User_Name );
+//void delete_User(string deleted_User);
+//void adjust_User_Balance(string adjusted_user_name,int value);
+//int get_Index(string user_name);
+//};
