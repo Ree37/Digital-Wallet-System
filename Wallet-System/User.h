47878a16efb6a88de8148b92ae414f29a27b3a7a
Ree37 <rimo.mrben@gmail.com>
2024-04-16 18:11:22 +0200

    setting User.h members and functions

--- Wallet-System/User.h
+++ Wallet-System/User.h
@@ -1,11 +1,43 @@
 #pragma once
 #include <vector>
+#include <string>
+using namespace std;
+
+class Transaction;
+
 class User
 {
+private:
+	string username;
+	string password;
+	float balance;
+	static vector<User*> users;
+	vector<Transaction> sentTransictions;
+	vector<Transaction> receivedTransactions;
 public:
-    string name;
-    bool susbended_Flag;
-    int balance;
-   vector<int> transactions;
+	User(string username, string password);
+	string getUsername();
+	string getPassword();
+	float getBalance();
+	void setUsername(string newUsername);
+	void setPassword(string newPassword);
+	bool strongPassword();
+	bool isUniqueUsername();
+	void addUser();
+
+};
+
+
+class Transaction {
+private:
+	User *sender;
+	User *recipient;
+	float amount;
+public:
+	Transaction(User sender, User recipient, float amount);
+	bool checkSenderBalance();
+	void sendAmount();
+	void addToSentTransactions();
+	void addToReceivedTransactions();
 };
 
