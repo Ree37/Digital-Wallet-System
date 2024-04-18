e38b606b024831fd1e66d2702172374a1398f1c6
Bebo-91 <abanobhany91@gmail.com>
2024-04-18 05:27:54 +0200

    commenting admin.cpp till resolving errors

--- Wallet-System/Admin.cpp
+++ Wallet-System/Admin.cpp
@@ -1,53 +1,53 @@
-#include "Admin.h"
-#include "User.h"
-
-int getIndex(string user_name) {
-	for (int i = 0; i <= users.size(); i++) {
-		if (users[i].name == user_name) {
-			return i;
-		}
-		else {
-			continue;
-		}
-	}
-}
-
-void deleteUser(string deletedUserName) {
-
-	int position = getIndex(deletedUserName);
-	users.erase(position);
-	uniqueUserName.erase(deletedUserName);
-}
-
-void suspendUsers(string suspendedUserName) {
-	int position = getIndex(suspendedUserName);
-	users.at(position).susbended_Flag = true;
-}
-
-void adjustUserBalance(string adjustedUserName, int value) {
-	int position = getIndex(adjustedUserName);
-	users.at(position).balance = value;
-}
-
-//void viewUsers()
-//{
+//#include "Admin.h"
+//#include "User.h"
+//
+//int getIndex(string user_name) {
 //	for (int i = 0; i <= users.size(); i++) {
-//		cout << "Username : " << users.at(i).name << "\t Balance : " << users.at(i).balance
-//			<< endl;
-//		for (int j = 0; j < users.at(i).transactions.size(); i++)
-//		{
-//			cout << "Transaction " << j + 1 << " :" << users.at(i).transactions.at(j);
+//		if (users[i].name == user_name) {
+//			return i;
 //		}
-//		cout << "************************************************************************";
+//		else {
+//			continue;
+//		}
 //	}
 //}
-vector<User> viewUsers()
-{
-	vector<User> userVec;
-	for (int i = 0; i <= users.size(); i++) {
-		userVec.push_back(users.at(i))
-	}
-	return userVec;
-}
-
-bool isUnique(string name) { return !uniqueUserName.count(name); }
+//
+//void deleteUser(string deletedUserName) {
+//
+//	int position = getIndex(deletedUserName);
+//	users.erase(position);
+//	uniqueUserName.erase(deletedUserName);
+//}
+//
+//void suspendUsers(string suspendedUserName) {
+//	int position = getIndex(suspendedUserName);
+//	users.at(position).susbended_Flag = true;
+//}
+//
+//void adjustUserBalance(string adjustedUserName, int value) {
+//	int position = getIndex(adjustedUserName);
+//	users.at(position).balance = value;
+//}
+//
+////void viewUsers()
+////{
+////	for (int i = 0; i <= users.size(); i++) {
+////		cout << "Username : " << users.at(i).name << "\t Balance : " << users.at(i).balance
+////			<< endl;
+////		for (int j = 0; j < users.at(i).transactions.size(); i++)
+////		{
+////			cout << "Transaction " << j + 1 << " :" << users.at(i).transactions.at(j);
+////		}
+////		cout << "************************************************************************";
+////	}
+////}
+//vector<User> viewUsers()
+//{
+//	vector<User> userVec;
+//	for (int i = 0; i <= users.size(); i++) {
+//		userVec.push_back(users.at(i));
+//	}
+//	return userVec;
+//}
+//
+//bool isUnique(string name) { return !uniqueUserName.count(name); }
