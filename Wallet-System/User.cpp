02d686b50ba52d2593c93738dc81bff0a5970bb9
Ree37 <rimo.mrben@gmail.com>
2024-04-18 16:56:37 +0200

    Definning addUser function in User.cpp

--- Wallet-System/User.cpp
+++ Wallet-System/User.cpp
@@ -1,25 +1,29 @@
 #include "User.h"
 #include "Security/Utils.h"
 #include <iostream>
+using namespace std;
 
-User::User(std::string username, std::string password) {
-    // Input Validation: check if username or password is empty
-    if (username.empty() || password.empty()) {
-        std::cerr << "Error: Username or password is empty." << std::endl;
-        return;
-    }
+User::User(string username, string password) {
+	// Input Validation: check if username or password is empty
+	if (username.empty() || password.empty()) {
+		cerr << "Error: Username or password is empty." << endl;
+		return;
+	}
 
-    // Input Validation: check if password matches a good password policy
-    if(!Utils::checkPasswordPolicy(password))
-    {
-        std::cout << "Password is weak. It should include an uppercase letter, a lowercase letter, a number, a special character, and be at least 8 characters long." << endl;
-        return;
-    }
-    this->susbended_Flag = false;
-    this->balance = 0.0;
-    this->username = username;
-    this->password = password;
+	// Input Validation: check if password matches a good password policy
+	if (!Utils::checkPasswordPolicy(password))
+	{
+		cout << "Password is weak. It should include an uppercase letter, a lowercase letter, a number, a special character, and be at least 8 characters long." << endl;
+		return;
+	}
+	this->susbended_Flag = false;
+	this->balance = 0.0;
+	this->username = username;
+	this->password = password;
 }
 float User::getBalance() { return balance; };
 string User::getUsername() { return username; };
 string User::getPassword() { return password; };
+void User::addUser() {
+	users.push_back(this);
+}
