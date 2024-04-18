9a1dac117095a3e525a8bfe62f6f8e02b483029e
Titoot <82727556+Titoot@users.noreply.github.com>
2024-04-18 06:42:22 +0200

    added basic User implementation, added basic Input Validations, added password policy check

--- Wallet-System/User.cpp
+++ Wallet-System/User.cpp
@@ -1,1 +1,25 @@
 #include "User.h"
+#include "Security/Utils.h"
+#include <iostream>
+
+User::User(std::string username, std::string password) {
+    // Input Validation: check if username or password is empty
+    if (username.empty() || password.empty()) {
+        std::cerr << "Error: Username or password is empty." << std::endl;
+        return;
+    }
+
+    // Input Validation: check if password matches a good password policy
+    if(!Utils::checkPasswordPolicy(password))
+    {
+        std::cout << "Password is weak. It should include an uppercase letter, a lowercase letter, a number, a special character, and be at least 8 characters long." << endl;
+        return;
+    }
+    this->susbended_Flag = false;
+    this->balance = 0.0;
+    this->username = username;
+    this->password = password;
+}
+float User::getBalance() { return balance; };
+string User::getUsername() { return username; };
+string User::getPassword() { return password; };
