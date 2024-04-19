1c42b5f687385b2a523f8b8fc55eecfa0d992822
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2024-04-20 01:47:02 +0200

    Revert "add Admin to functions and constructor and transac func"
    This reverts commit 4f43695da30e1806b060b8fa83af11d8ca718d03.

--- Wallet-System/Admin.cpp
+++ Wallet-System/Admin.cpp
@@ -1,49 +1,53 @@
 #include "Admin.h"
 #include "User.h"
-Admin::Admin(std::string name, std::string password) {
-    this->name=name;
-    this->password=password;
 
-}
-int Admin::getIndex(string user_name) {
-    for (int i = 0; i <= users.size(); i++) {
-        if (users[i].name == user_name) {
-            return i;
-        }
-        else {
-            continue;
-        }
-    }
+int getIndex(string user_name) {
+	for (int i = 0; i <= users.size(); i++) {
+		if (users[i].name == user_name) {
+			return i;
+		}
+		else {
+			continue;
+		}
+	}
 }
 
-void Admin::deleteUser(string deletedUserName) {
+void deleteUser(string deletedUserName) {
 
-    int position = getIndex(deletedUserName);
-    users.erase(position);
-    uniqueUserName.erase(deletedUserName);
+	int position = getIndex(deletedUserName);
+	users.erase(position);
+	uniqueUserName.erase(deletedUserName);
 }
 
-void Admin::suspendUsers(string suspendedUserName) {
-    int position = getIndex(suspendedUserName);
-    users.at(position).susbended_Flag = true;
+void suspendUsers(string suspendedUserName) {
+	int position = getIndex(suspendedUserName);
+	users.at(position).susbended_Flag = true;
 }
 
-void Admin::adjustUserBalance(string adjustedUserName, int value) {
-    int position = getIndex(adjustedUserName);
-    users.at(position).balance = value;
+void adjustUserBalance(string adjustedUserName, int value) {
+	int position = getIndex(adjustedUserName);
+	users.at(position).balance = value;
 }
 
-vector<User> Admin::viewUsers()
+//void viewUsers()
+//{
+//	for (int i = 0; i <= users.size(); i++) {
+//		cout << "Username : " << users.at(i).name << "\t Balance : " << users.at(i).balance
+//			<< endl;
+//		for (int j = 0; j < users.at(i).transactions.size(); i++)
+//		{
+//			cout << "Transaction " << j + 1 << " :" << users.at(i).transactions.at(j);
+//		}
+//		cout << "************************************************************************";
+//	}
+//}
+vector<User> viewUsers()
 {
-    vector<User> userVec;
-    for (int i = 0; i <= users.size(); i++) {
-        userVec.push_back(users.at(i))
-    }
-    return userVec;
-}
-vector<User> Admin::viewTransactions()
-{
-    return users;
+	vector<User> userVec;
+	for (int i = 0; i <= users.size(); i++) {
+		userVec.push_back(users.at(i))
+	}
+	return userVec;
 }
 
-bool Admin::isUnique(string name) { return !uniqueUserName.count(name); }
+bool isUnique(string name) { return !uniqueUserName.count(name); }
