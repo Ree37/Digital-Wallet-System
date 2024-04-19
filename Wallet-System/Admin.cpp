ca3410d056ac0fcb51d4e819b83e5a4245f9be3f
jupiter <2022170114@cis.asu.edu.eg>
2024-04-19 22:49:42 +0200

    add Admin:: in all functions

--- Wallet-System/Admin.cpp
+++ Wallet-System/Admin.cpp
@@ -1,7 +1,7 @@
 #include "Admin.h"
 #include "User.h"
 
-int getIndex(string user_name) {
+int Admin::getIndex(string user_name) {
 	for (int i = 0; i <= users.size(); i++) {
 		if (users[i].name == user_name) {
 			return i;
@@ -12,36 +12,23 @@ int getIndex(string user_name) {
 	}
 }
 
-void deleteUser(string deletedUserName) {
+void Admin::deleteUser(string deletedUserName) {
 
 	int position = getIndex(deletedUserName);
 	users.erase(position);
 	uniqueUserName.erase(deletedUserName);
 }
 
-void suspendUsers(string suspendedUserName) {
+void Admin::suspendUsers(string suspendedUserName) {
 	int position = getIndex(suspendedUserName);
 	users.at(position).susbended_Flag = true;
 }
 
-void adjustUserBalance(string adjustedUserName, int value) {
+void Admin::adjustUserBalance(string adjustedUserName, int value) {
 	int position = getIndex(adjustedUserName);
 	users.at(position).balance = value;
 }
-
-//void viewUsers()
-//{
-//	for (int i = 0; i <= users.size(); i++) {
-//		cout << "Username : " << users.at(i).name << "\t Balance : " << users.at(i).balance
-//			<< endl;
-//		for (int j = 0; j < users.at(i).transactions.size(); i++)
-//		{
-//			cout << "Transaction " << j + 1 << " :" << users.at(i).transactions.at(j);
-//		}
-//		cout << "************************************************************************";
-//	}
-//}
-vector<User> viewUsers()
+vector<User>  Admin::viewUsers()
 {
 	vector<User> userVec;
 	for (int i = 0; i <= users.size(); i++) {
