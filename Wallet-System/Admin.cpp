4f43695da30e1806b060b8fa83af11d8ca718d03
jupiter <2022170114@cis.asu.edu.eg>
2024-04-19 23:27:58 +0200

    add Admin to functions and constructor and transac func

--- Wallet-System/Admin.cpp
+++ Wallet-System/Admin.cpp
@@ -1,53 +1,49 @@
 #include "Admin.h"
 #include "User.h"
+Admin::Admin(std::string name, std::string password) {
+    this->name=name;
+    this->password=password;
 
-int getIndex(string user_name) {
-	for (int i = 0; i <= users.size(); i++) {
-		if (users[i].name == user_name) {
-			return i;
-		}
-		else {
-			continue;
-		}
-	}
+}
+int Admin::getIndex(string user_name) {
+    for (int i = 0; i <= users.size(); i++) {
+        if (users[i].name == user_name) {
+            return i;
+        }
+        else {
+            continue;
+        }
+    }
 }
 
-void deleteUser(string deletedUserName) {
+void Admin::deleteUser(string deletedUserName) {
 
-	int position = getIndex(deletedUserName);
-	users.erase(position);
-	uniqueUserName.erase(deletedUserName);
+    int position = getIndex(deletedUserName);
+    users.erase(position);
+    uniqueUserName.erase(deletedUserName);
 }
 
-void suspendUsers(string suspendedUserName) {
-	int position = getIndex(suspendedUserName);
-	users.at(position).susbended_Flag = true;
+void Admin::suspendUsers(string suspendedUserName) {
+    int position = getIndex(suspendedUserName);
+    users.at(position).susbended_Flag = true;
 }
 
-void adjustUserBalance(string adjustedUserName, int value) {
-	int position = getIndex(adjustedUserName);
-	users.at(position).balance = value;
+void Admin::adjustUserBalance(string adjustedUserName, int value) {
+    int position = getIndex(adjustedUserName);
+    users.at(position).balance = value;
 }
 
-//void viewUsers()
-//{
-//	for (int i = 0; i <= users.size(); i++) {
-//		cout << "Username : " << users.at(i).name << "\t Balance : " << users.at(i).balance
-//			<< endl;
-//		for (int j = 0; j < users.at(i).transactions.size(); i++)
-//		{
-//			cout << "Transaction " << j + 1 << " :" << users.at(i).transactions.at(j);
-//		}
-//		cout << "************************************************************************";
-//	}
-//}
-vector<User> viewUsers()
+vector<User> Admin::viewUsers()
 {
-	vector<User> userVec;
-	for (int i = 0; i <= users.size(); i++) {
-		userVec.push_back(users.at(i))
-	}
-	return userVec;
+    vector<User> userVec;
+    for (int i = 0; i <= users.size(); i++) {
+        userVec.push_back(users.at(i))
+    }
+    return userVec;
+}
+vector<User> Admin::viewTransactions()
+{
+    return users;
 }
 
-bool isUnique(string name) { return !uniqueUserName.count(name); }
+bool Admin::isUnique(string name) { return !uniqueUserName.count(name); }
--- Wallet-System/Admin.h
+++ Wallet-System/Admin.h
@@ -1,24 +1,37 @@
 #pragma once
+
 #include<iostream>
 #include<string>
 #include<set>
 #include<vector>
 #include "User.h"
+
 using namespace std;
 
-class Admin
-{
-string name;
-string password;
-vector<User> users;
-set<string> uniqueUserName;
-bool isUnique(string name);
-void addUser();
-void viewUsers();
-void editUsers(User edited_User);
-void viewTransactions();
-void suspendUsers(string suspended_User_Name );
-void deleteUser(string deleted_User);
-void adjustUserBalance(string adjusted_user_name,int value);
-int getIndex(string user_name);
+class Admin {
+private:
+    string name;
+    string password;
+    vector <User> users;
+    set <string> uniqueUserName;
+public:
+    Admin(string name, string password);
+
+    bool isUnique(string name);
+
+    void addUser();
+
+    void viewUsers();
+
+    void editUsers(User edited_User);
+
+    void viewTransactions();
+
+    void suspendUsers(string suspended_User_Name);
+
+    void deleteUser(string deleted_User);
+
+    void adjustUserBalance(string adjusted_user_name, int value);
+
+    int getIndex(string user_name);
 };
